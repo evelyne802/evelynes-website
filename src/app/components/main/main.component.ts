3d2ec8cb5af518d0d3fd7065ef8ec55a8230e049
evelyne802 <linagluzman@gmail.com>
2024-11-04 18:21:56 +0200

    fix: progress bar design

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -28,7 +28,6 @@ export class MainComponent {
     let backdrop = document.getElementById('backdrop');
 
     progeressBar?.addEventListener('animationend', function() {
-      console.log('blabla');
       setTimeout(function() {
         progeressBar!.className = "hidden";
       }, 30)
